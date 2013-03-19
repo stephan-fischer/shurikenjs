1e64f5e1c9561752fe71788d9b3dfdfea1101775
Evan Hahn <me@evanhahn.com>
2013-03-19 16:24:11 -0400

    Add another test for document.create

--- test/spec/spec.js
+++ test/spec/spec.js
@@ -6,12 +6,23 @@ describe("shuriken", function() {
 
 		describe("create", function() {
 			it("can make simple, empty elements", function() {
-				var div = document.create("div");
-				var span = document.create("span");
-				var a = document.create("a");
-				expect(div).toEqual(jasmine.any(Node));
-				expect(span).toEqual(jasmine.any(Node));
-				expect(a).toEqual(jasmine.any(Node));
+				var div = document.create("<div>");
+				var span = document.create("<span></span>");
+				var a = document.create("<a>");
+				expect(div).toEqual(jasmine.any(HTMLDivElement));
+				expect(span).toEqual(jasmine.any(HTMLSpanElement));
+				expect(a).toEqual(jasmine.any(HTMLAnchorElement));
+			});
+			it("can make elements with attributes", function() {
+				var div = document.create('<div id="foo">');
+				var span = document.create('<span class="bar"></span>');
+				var a = document.create('<a href="http://example.com/">');
+				expect(div).toEqual(jasmine.any(HTMLDivElement));
+				expect(div.id).toEqual("foo");
+				expect(span).toEqual(jasmine.any(HTMLSpanElement));
+				expect(span.className).toEqual("bar");
+				expect(a).toEqual(jasmine.any(HTMLAnchorElement));
+				expect(a.href).toEqual("http://example.com/");
 			});
 		});
 
